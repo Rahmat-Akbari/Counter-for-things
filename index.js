c22187c1dca906ad3bdf223794bcebe4bf0b3e5a
Rahmat Akbari <rahmat.akbari@hotmail.com>
2023-10-31 12:22:12 +0200

    didnt do a thing

---
+++ index.js
@@ -0,0 +1,23 @@
+//let resetEl = document.getElementById("reset-el")
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+function reset() {
+    let countStr = count
+    countEl.textContent = 0
+    count = 0
+    saveEl.textContent ="Previous entries:"
+}
