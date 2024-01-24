919f52ef64f5f7a0500df42d87f2332e1815c5e1
Rahmat Akbari <rahmat.akbari@hotmail.com>
2024-01-24 14:33:30 +0200

    added new decrement button and fixed some bugs

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
-let totalEl = document.getElementById("total-el")
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let sumResultEl = document.getElementById("sumResult");
+let count = 0;
+let saveValues = [];
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+    count += 1;
+    countEl.textContent = count;
+}
+
+function decrement() {
+    if (count > 0) {
+        count -= 1;
+        countEl.textContent = count;
+    }
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+    if (count > 0){  //only saves greater than 0
+        let countValue = count;
+        saveValues.push(countValue); // Save the count value in the array
+        let countStr = countValue + " - ";
+        saveEl.textContent += countStr;
+        countEl.textContent = 0;
+        count = 0;
+    }
 }
 
 function reset() {
-    let countStr = count
-    countEl.textContent = 0
-    count = 0
-    saveEl.textContent ="Previous entries: "
+    countEl.textContent = 0;
+    count = 0;
+    saveEl.textContent = "Previous entries: ";
+    sumResultEl.innerText = "Sum: ";
+    saveValues = []; // Reset the array when resetting
 }
 
-function total(){
-    totalEl.textContent += countStr
+function sumAllSaved() {
+    const sum = saveValues.reduce((acc, val) => acc + val, 0); // Calculate the sum easily
+    sumResultEl.innerText = `Sum: ${sum}`;
 }
